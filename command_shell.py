31f7dbabd530a81368857da4255d17e807b085bf
albedimaria <dimaria.alberto@outlook.com>
2025-07-01 18:25:13 +0200

    command_shell added

---
+++ command_shell.py
@@ -0,0 +1,4 @@
+"""
+keep the suggested format
+"""
+# python generate.py --model nameModel.ts --mode encode --input_file nameInput.wav --noise 2.2 --scale 1.0 1.2 1.5 1.0 1.2 1.5 1.0 1.2 1.5 1.0 1.2 1.5 1.0 1.2 1.5 1.0 1.3 0.9 1.6 1.1 --bias 0.0 0.1 0.2 0.0 0.1 0.2 0.0 0.1 0.2 0.0 0.1 0.2 0.0 0.1 0.2 0.0 -0.1 0.05 -0.2 0.15 --output_file output.wav
