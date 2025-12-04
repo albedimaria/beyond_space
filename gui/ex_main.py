20250b5d2fcc74bd9a0fc51e053b208450f7a3bc
albedimaria <dimaria.alberto@outlook.com>
2025-12-04 20:40:38 +0100

    updated

--- gui/ex_main.py
+++ gui/ex_main.py
@@ -6,7 +6,7 @@ import streamlit as st
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from timeline import render_timeline
-from app_def.processing import process_files
+from app.processing import process_files
 
 # ==========================
 # ui: page reader
